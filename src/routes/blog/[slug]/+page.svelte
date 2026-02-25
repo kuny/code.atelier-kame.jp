e15793ed457fbd873f37a8a4e6d24428613e49a9
Kunihisa Matsuda <love_coke@mac.com>
2026-02-25 19:37:22 +0900

    +page.svelte

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -7,7 +7,11 @@
   };
 </script>
 
-<h1>{data.title}</h1>
+<h1>
+(atelier-kame 'code
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'blog)
+</h1>
+<h2>{data.title}</h2>
 {#if data.date}<p><small>{data.date}</small></p>{/if}
 
 <pre style="white-space: pre-wrap; line-height: 1.6;">{data.content}</pre>
