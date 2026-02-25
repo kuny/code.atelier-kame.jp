56ffbefea084957b73cc37bfdd4b559d20cb0c72
Kunihisa Matsuda <love_coke@mac.com>
2026-02-25 11:45:11 +0900

    add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+    preprocess: [
+        vitePreprocess(),
+        mdsvex({
+            extensions: ['.svx', '.md']
+        })
+    ],
 	kit: {
 		// adapter-auto から adapter-static に変更
 		adapter: adapter({
@@ -13,21 +19,10 @@ const config = {
 			precompress: false,
 			strict: true
 		})
-	}
+	},
+
+    extensions: ['.svelte', '.svx', '.md']
 };
 
 export default config;
 
-// import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
-// const config = {
-	// kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		// adapter: adapter()
-	// }
-// };
-
-// export default config;
